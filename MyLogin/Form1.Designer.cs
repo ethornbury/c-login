d73f80d36411c4afeb95819785b73dc2eaae3833
Emer Thornbury <10330166@mydbs.ie>
2016-02-02 20:52:53 +0000

    basic form laid out

---
+++ MyLogin/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace MyLogin
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtBxUser = new System.Windows.Forms.TextBox();
+            this.txtBxPass = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(25, 33);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(61, 13);
+            this.lblUser.TabIndex = 0;
+            this.lblUser.Text = "Username: ";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(28, 76);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(59, 13);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Password: ";
+            // 
+            // txtBxUser
+            // 
+            this.txtBxUser.Location = new System.Drawing.Point(133, 33);
+            this.txtBxUser.Name = "txtBxUser";
+            this.txtBxUser.Size = new System.Drawing.Size(100, 20);
+            this.txtBxUser.TabIndex = 2;
+            // 
+            // txtBxPass
+            // 
+            this.txtBxPass.Location = new System.Drawing.Point(133, 68);
+            this.txtBxPass.Name = "txtBxPass";
+            this.txtBxPass.Size = new System.Drawing.Size(100, 20);
+            this.txtBxPass.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(99, 138);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 203);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtBxPass);
+            this.Controls.Add(this.txtBxUser);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUser);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtBxUser;
+        private System.Windows.Forms.TextBox txtBxPass;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ MyLogin/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyLogin
+{
+    public partial class Login : Form
+    {
+        string username = "admin";
+        string password = "password";
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string user = txtBxUser.Text;
+            string pass = txtBxPass.Text;
+
+            if(user == username)
+            {
+                if(pass == password)
+                {
+                    txtBxPass.Clear();
+                    txtBxUser.Clear();
+                    MessageBox.Show("You are logged in!");                    
+                }
+                else
+                {
+                    txtBxPass.Clear();
+                    txtBxUser.Clear();
+                    MessageBox.Show("Password is wrong");                   
+                }
+            }
+            else
+            {
+                txtBxPass.Clear();
+                txtBxUser.Clear();
+                MessageBox.Show("Username is wrong");
+            }
+            
+        }
+    }
+}
